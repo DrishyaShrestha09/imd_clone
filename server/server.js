e9f415740c4f09326fa99de90d26f60f2509ea0c
Ridan Shrestha <ridanstha09@gmail.com>
2024-11-11 19:36:20 +0545

    imported movieRoutes in server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const dbConnect = require("./dbConnect")
+const movieRoutes = rewuire("/.routes/movies");
 const cors = require("cors");
 const app = express();
 
@@ -9,6 +10,8 @@ dbConnect();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api", movieRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on port ${PORT}...`));
