da01ac29e851572ba6b265858d24201c5b01f9de
Ridan Shrestha <ridanstha09@gmail.com>
2024-11-13 21:43:38 +0545

    fixed minor bug

--- client/src/App.js
+++ client/src/App.js
@@ -5,24 +5,24 @@ import "./App.css";
 const base_url = process.env.REACT_APP_API_URL;
 function App() {
   const [obj, setObj] = useState({});
-  const [sort, setSort] = useState({ sort: "rating", order: "desc"});
+  const [sort, setSort] = useState({ sort: "rating", order: "desc" });
   const [filterGenre, setFilterGenre] = useState([]);
   const [page, setPage] = useState(1);
   const [search, setSearch] = useState("");
 
-useEffect(() => {
-  const getAllMovies = async () => {
-    try {
-      const url = `${base_url}?page=${page}&sort=${sort.sort}, ${sort.order} &genre=${filterGenre.toString()}&search=${search}`;
-      const { data } = await axios.get(url);
-      setObj(data);
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  getAllMovies();
-}, [sort, filterGenre, page, search]);
+  useEffect(() => {
+    const getAllMovies = async () => {
+      try {
+        const url = `${base_url}?page=${page}&sort=${sort.sort},${sort.order}&genre=${filterGenre.toString()}&search=${search}`;
+        const { data } = await axios.get(url);
+        setObj(data);
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getAllMovies();
+  }, [sort, filterGenre, page, search]);
 
   return <h1>Hello world</h1>;
 }
