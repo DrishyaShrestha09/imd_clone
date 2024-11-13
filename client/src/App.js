a4a516d526c8e10f47df9e095a1686d6dee927fe
Ridan Shrestha <ridanstha09@gmail.com>
2024-11-13 21:04:17 +0545

    Adding api

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
+const base_url = process.env.REACT_APP_API_URL;
 function App() {
-  return (
-  <h1>Hello world</h1>
-  );
+  const [obj, setObj] = useState({});
+  const [sort, setSort] = useState({ sort: "rating", order: "desc"});
+  const [filterGenre, setFilterGenre] = useState([]);
+  const [page, setPage] = useState(1);
+  const [search, setSearch] = useState("");
+
+useEffect(() => {
+  const getAllMovies = async () => {
+    try {
+      const url = `${base_url}?page=${page}&sort=${sort.sort}, ${sort.order} &genre=${filterGenre.toString()}&search=${search}`;
+      const { data } = await axios.get(url);
+      setObj(data);
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getAllMovies();
+}, [sort, filterGenre, page, search]);
+
+  return <h1>Hello world</h1>;
 }
 
 export default App;
